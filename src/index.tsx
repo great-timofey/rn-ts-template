1af60b3fdb1d7d0df467e6646ae5f390b5f02bb9
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-02-14 12:58:10 +0700

    Last init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // app entry
 import { AppRegistry } from "react-native";
 import { Config } from "./commons";
-import { App } from "./containers";
+import { App } from "$containers";
 
 // register the app
 AppRegistry.registerComponent(Config.app.name, () => App);
