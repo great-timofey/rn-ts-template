ee5ca03819fe0247146274ea18eb3bb8d0d40fdf
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2019-02-14 12:40:25 +0700

    Lint checking

--- src/components/MyComponent/index.tsx
+++ src/components/MyComponent/index.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import { StyleSheet, Text } from "react-native";
 
 const styles = StyleSheet.create({
-    welcome: {
-        color: "red",
-          marginBottom: 5,
-      textAlign: "center"
-    },
-})
+  welcome: {
+    color: "red",
+    marginBottom: 5,
+    textAlign: "center"
+  }
+});
 
 export const MyComponent = () => (
-    <Text style={styles.welcome}>This is my first component!</Text>
-)
+  <Text style={styles.welcome}>This is my first component!</Text>
+);
